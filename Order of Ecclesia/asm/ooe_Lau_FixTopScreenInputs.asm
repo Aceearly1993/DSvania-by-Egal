c577dc222d0f4b950ab72dbcb8998d986e2abf58
EgalLau37 <EgalLau37@gmail.com>
2025-08-18 21:25:50 -0500

    081820205 update2

--- Order of Ecclesia/asm/ooe_Lau_FixTopScreenInputs.asm
+++ Order of Ecclesia/asm/ooe_Lau_FixTopScreenInputs.asm
@@ -12,7 +12,7 @@
 ; by EgalLau37
 
 Overlay86Start equ 0x22EB1A0
-FreeSpace equ Overlay86Start+0x2840
+FreeSpace equ Overlay86Start+0x2000
 
 
 .open "ftc/arm9.bin", 0x2000000
@@ -20,8 +20,6 @@ FreeSpace equ Overlay86Start+0x2840
 .org 0x20AF4E4
 mov r0,r9
 bl RetrieveExactSameInputs
-.org 0x20AF500	;always update; probably not necessary, but just in case
-mov r3,r3
 
 .close
 
@@ -32,15 +30,26 @@ mov r3,r3
 .org FreeSpace
 
 RetrieveExactSameInputs:
-ldr r1,=TableOfCombatInputs
+ldr r1,=0x2100790
+ldrb r1,[r1,0x2]
+cmp r1,0x4
+ldrne r1,=TableOfCombatInputs
+ldreq r1,=TableOfCombatInputs_Albus
 ldr r0,[r1,r0,lsl 0x2]
 bx r14
 .pool
+
 
 TableOfCombatInputs:
 .dw 0x436	;text ID for Y button
 .dw 0x435	;text ID for X button
 .dw 0x438	;text ID for R button
+.dw 0x61D
 
+TableOfCombatInputs_Albus:
+.dw 0x438	;text ID for R button
+.dw 0x61D	;text ID for glyph union
+.dw 0x436	;text ID for Y button
+.dw 0x435	;text ID for X button
 
 .close
